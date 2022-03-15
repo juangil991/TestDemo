06b33efe1628fc78efbf6fcc5c39e250a0574d09
juangil991 <jcgo125@gmail.com>
2022-03-15 10:01:44 -0500

    Se hacer test de la Resta

--- src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
+++ src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
@@ -11,4 +11,23 @@ public class BasicCalculator {
         return number1 + number2;
     }
 
+    public Long rest(Long number1, Long number2){
+        logger.info("subtracting {} - {}",number1,number2);
+        return number1 - number2;
+    }
+
+    public Float div(Float number1 , Float number2){
+        logger.info("dividing {} / {}",number1,number2);
+        if(number2 !=0 ){return  number1/number2;}
+        else {
+            System.out.println("no se puede dividir por 0");
+            return 0f;
+        }
+    }
+
+    public Long mul(Long number1,Long number2){
+        logger.info("multiplying {} * {} ",number1,number2);
+        return number1*number2;
+    }
+
 }
---
+++ src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
@@ -0,0 +1,40 @@
+package co.com.sofka.app.calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasicCalculatorTest {
+    private final BasicCalculator basicCalculator = new BasicCalculator();
+
+
+    @DisplayName("Testing several sums")
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+    public void severalSums(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.sum(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+    }
+
+    @DisplayName("Testing several rest")
+    @ParameterizedTest(name = "{0} - {1} = {2}")
+    @CsvSource({
+            "0,    1,   -1",
+            "1,    2,   -1",
+            "49,  51, -2",
+            "1,  100, -99"
+    })
+    public void severalRest(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.rest(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+    }
+
+}
